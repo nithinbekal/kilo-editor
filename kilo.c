25eea9d9249e97337d27193b595b1cdc0ab50b67
Nithin Bekal <nithinbekal@gmail.com>
2017-04-14 18:01:12 +0530

    Use C-q to quit

--- kilo.c
+++ kilo.c
@@ -5,6 +5,8 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 struct termios orig_termios;
 
 void die(const char* s) {
@@ -45,7 +47,7 @@ int main() {
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
-    if (c == 'q') break;
+    if (c == CTRL_KEY('q')) break;
   }
 
   return 0;
