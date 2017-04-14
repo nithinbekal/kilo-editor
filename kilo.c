ee8205a7687c1ee0433451486f8053946ac1b7a2
Nithin Bekal <nithinbekal@gmail.com>
2017-04-14 22:27:28 +0530

    Calculate window size

--- kilo.c
+++ kilo.c
@@ -2,14 +2,19 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
+
+/* Defines */
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /* Data */
 
 struct editorConfig {
+  int screenRows;
+  int screenCols;
   struct termios orig_termios;
 };
 
@@ -58,6 +63,39 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDIN_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+  return 0;
+}
+
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    editorReadKey();
+    return getCursorPosition(rows, cols);
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /* Input */
 
 void editorProcessKeyPress() {
@@ -77,8 +115,10 @@ void editorProcessKeyPress() {
 
 void editorDrawRows() {
   int y;
-  for (y = 0; y < 24; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+  for (y = 0; y < E.screenRows; y++) {
+    write(STDOUT_FILENO, "~", 1);
+
+    if (y < E.screenRows -1) write(STDOUT_FILENO, "\r\n", 2);
   }
 }
 
@@ -96,9 +136,17 @@ void editorRefreshScreen() {
   write(STDIN_FILENO, "\x1b[H", 3);
 }
 
+/* Init */
+
+void initEditor() {
+  if (getWindowSize(&E.screenRows, &E.screenCols) == -1)
+    die("getWindowSize");
+}
+
 
 int main() {
   enableRawMode();
+  initEditor();
 
   while (1) {
     editorRefreshScreen();
