104535e8ec40f256acff2da5a95480aab1106e99
Nithin Bekal <nithinbekal@gmail.com>
2017-04-14 17:59:27 +0530

    Cleanup

--- kilo.c
+++ kilo.c
@@ -36,16 +36,14 @@ void enableRawMode() {
 int main() {
   enableRawMode();
 
-  char c;
-
   while (1) {
     char c = '\0';
     if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
 
-    if (isprint(c)) {
-      printf("%d ('%c')\r\n", c, c);
-    } else {
+    if (iscntrl(c)) {
       printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
     }
     if (c == 'q') break;
   }
